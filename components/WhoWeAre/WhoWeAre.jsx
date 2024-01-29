0c811813bb447bcc46429ec47ad11c6b85af53ea
Ammar1122s <ammaraamir33@gmail.com>
2024-01-29 19:55:27 +0500

    new

--- components/WhoWeAre/WhoWeAre.jsx
+++ components/WhoWeAre/WhoWeAre.jsx
@@ -21,7 +21,7 @@ const WhoWeAre = () => {
               <div className="section-title mb-30">
                 <span className="sub-title">Who we are</span>
                 <h2 className="title">
-                  The World’s <span>1st ICO</span> Platform That Offers Rewards
+                  The World’s <span>12st ICO</span> Platform That Offers Rewards
                 </h2>
               </div>
               <p>
